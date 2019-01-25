c82cd05bc04d9fe7ec5e70398ac35cd1fd63397e
Shoichiro Sakaigawa <s.sakaigawa@pumpkinheads.jp>
2019-01-25 16:10:03 +0900

    dump

--- config/bluemix.js
+++ config/bluemix.js
@@ -9,11 +9,18 @@
  * name is not found in VCAP_SERVICES
  */
 module.exports.getServiceCreds = function(name) {
+    // utilモジュールを使います。
+    var util = require('util');
+    function dump(v) {
+        return console.log(util.inspect(v));
+    }
+    
     if (process.env.VCAP_SERVICES) {
         var services = JSON.parse(process.env.VCAP_SERVICES);
         for (var service_name in services) {
             if (service_name.indexOf(name) === 0) {
                 var service = services[service_name][0];
+                dump(service);
                 return {
                     url: service.credentials.url,
                     username: service.credentials.username,
